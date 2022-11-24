f8879203868fe42d066a014ee4b08db85b7a2e03
asad0306 <qaisansari46@gamil.com>
2022-11-24 16:09:11 +0100

    Solve all 2 exercises by my method and check online as well to know more aboutit.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,25 @@
-const reverseString = function() {
 
-};
+const reverseString = function(str) {
+    let split = str.split("");
+    let reverseSplit = split.reverse();
+    let join = reverseSplit.join("");
+
+    return join;
+}
+
 
 // Do not edit below this line
 module.exports = reverseString;
+
+/*const reverseString = function(reverse) {
+if (reverse === "hello") {
+     return "olleh";
+} else if (reverse === "hello there") {
+    return "ereht olleh";
+} else if (reverse === "123! abc!") {
+    return "!cba !321"
+} else if (reverse === "") {
+    return "";
+}
+
+};*/
