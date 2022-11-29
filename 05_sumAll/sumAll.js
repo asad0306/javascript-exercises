b5e4f2bad43ef19a8ee9b14f184f6f47b67eb75a
asad0306 <qaisansari46@gamil.com>
2022-11-29 14:41:45 +0100

    exercise 05 / 06 all done by searching google/dicord and done and learn

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    if (firstNum < 0 || lastNum < 0 || typeof firstNum != 'number' || typeof lastNum != 'number') {
+        return 'ERROR'
+    } else if (firstNum < lastNum) {
+        for (let i = firstNum; i <= lastNum; i++) {
+            firstNum += i;
+        }
+        return firstNum -1;
+    } else if (lastNum < firstNum) {
+        for(let i = lastNum; i <= firstNum; i++) {
+            lastNum += 1
+        }
+         return lastNum -1;
+    }
 
 };
 
