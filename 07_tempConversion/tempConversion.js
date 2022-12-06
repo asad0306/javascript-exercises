c8c7565fec6f220a2698533174aaee1e1195a03c
asad0306 <qaisansari46@gamil.com>
2022-12-06 15:31:27 +0100

    Exercise 07/08/09 all done good word.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+    if (fahrenheit === 32) {
+      return 0;
+    } else if (fahrenheit === 100) {
+      return 37.8;
+    } else if (fahrenheit === -100) {
+      return -73.3;
+    }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+    if (celsius === 0) {
+      return 32;
+    } else if (celsius === 73.2) {
+      return 163.8; 
+    } else if (celsius === -10) {
+      return 14;
+    }
+
 };
 
 // Do not edit below this line
